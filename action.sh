5dc5c72f4c81daf1bf6facfc55f288d474173207
matijs <matijs@gmail.com>
2025-02-16 12:05:11 -0200

    Directly use HEAD

--- action.sh
+++ action.sh
@@ -3,13 +3,10 @@
 set -euxo pipefail
 
 BASE_REF=$(git rev-parse --verify "origin/${GITHUB_BASE_REF}^{commit}")
-#HEAD_REF=$(git rev-parse --verify "origin/${GITHUB_HEAD_REF}^{commit}")
+HEAD_REF=$(git rev-parse --verify "HEAD^{commit}")
 
-git log --pretty="%H %s"
 echo "BASE_REF: ${BASE_REF}"
-echo "GITHUB_SHA: ${GITHUB_SHA}"
-echo "GITHUB_REF: ${GITHUB_REF}"
-MY_SHA=$(git rev-parse "${GITHUB_REF}^{commit}")
-echo "MY_SHA: ${MY_SHA}"
+echo "HEAD_REF: ${HEAD_REF}"
+git log --pretty="%H %s"
 
-! grep --quiet -E '^(amend|fixup|squash)!' <(git log --pretty=%s "${BASE_REF}..${GITHUB_SHA}")
+! grep --quiet -E '^(amend|fixup|squash)!' <(git log --pretty=%s "${BASE_REF}..${HEAD_REF}")
