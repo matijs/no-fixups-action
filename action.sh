c1212c77069dd3e4f56459122ede03a910ed76c8
matijs <matijs@gmail.com>
2025-02-16 11:51:13 -0200

    show github_ref

--- action.sh
+++ action.sh
@@ -8,5 +8,6 @@ BASE_REF=$(git rev-parse --verify "origin/${GITHUB_BASE_REF}^{commit}")
 git log --pretty="%H %s"
 echo "BASE_REF: ${BASE_REF}"
 echo "GITHUB_SHA: ${GITHUB_SHA}"
+echo "GITHUB_REF: ${GITHUB_REF}"
 
 ! grep --quiet -E '^(amend|fixup|squash)!' <(git log --pretty=%s "${BASE_REF}..${GITHUB_SHA}")
