99222b7d4963a096c9c8fbd9fe76a87221ac74d4
bot <161242389+1f929-bot@users.noreply.github.com>
2025-02-16 11:25:36 -0200

    Update action.sh
    Use GITHUB_SHA

--- action.sh
+++ action.sh
@@ -3,5 +3,7 @@
 set -euxo pipefail
 
 BASE_REF=$(git rev-parse --verify "origin/${GITHUB_BASE_REF}^{commit}")
-HEAD_REF=$(git rev-parse --verify "origin/${GITHUB_HEAD_REF}^{commit}")
-! grep --quiet -E '^(amend|fixup|squash)!' <(git log --pretty=%s "${BASE_REF}..${HEAD_REF}")
+
+git log --pretty=%s
+
+! grep --quiet -E '^(amend|fixup|squash)!' <(git log --pretty=%s "${BASE_REF}..${GITHUB_SHA}")
