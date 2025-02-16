89b9caedd18517f6277d1b9caab20ca4a0d73c15
matijs <matijs@gmail.com>
2025-02-16 14:20:24 +0100

    fixup! test: shas

--- action.sh
+++ action.sh
@@ -5,4 +5,6 @@ set -euxo pipefail
 # The action already makes sure that is is a pull request so GITHUB_BASE_REF is always set
 BASE_REF=$(git rev-parse --verify "origin/${GITHUB_BASE_REF}^{commit}")
 
+echo "${BASE_REF}"
+
 ! grep --quiet -E '^(amend|fixup|squash)!' <(git log --pretty=%s "${BASE_REF}..${GITHUB_SHA}")
