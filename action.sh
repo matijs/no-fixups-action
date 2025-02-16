efd672477cc002ba654d09e0434091fc272156d7
matijs <matijs@gmail.com>
2025-02-16 11:44:24 -0200

    Update action.sh

--- action.sh
+++ action.sh
@@ -3,11 +3,11 @@
 set -euxo pipefail
 
 BASE_REF=$(git rev-parse --verify "origin/${GITHUB_BASE_REF}^{commit}")
-HEAD_REF=$(git rev-parse --verify "origin/${GITHUB_HEAD_REF}^{commit}")
+#HEAD_REF=$(git rev-parse --verify "origin/${GITHUB_HEAD_REF}^{commit}")
 
 git log --pretty="%H %s"
 echo "${BASE_REF}"
 echo "${HEAD_REF}"
 echo "${GITHUB_SHA}"
 
-! grep --quiet -E '^(amend|fixup|squash)!' <(git log --pretty=%s "${BASE_REF}..${HEAD_REF}")
+! grep --quiet -E '^(amend|fixup|squash)!' <(git log --pretty=%s "${BASE_REF}..${GITHUB_SHA}")
